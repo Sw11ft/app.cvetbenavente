ff5af156ddd43cdebe629ff8294e757b7d635317
Pedro G <pedro-gaspar@outlook.com>
2017-06-25 14:42:35 +0100

    Add project files.

---
+++ app.cvetbenavente/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace app.cvetbenavente
+{
+    [Activity(Label = "app.cvetbenavente", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
